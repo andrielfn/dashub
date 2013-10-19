5b6fae48e3e4d62dfe5702e71bfb36b16c674f8d
Fabio Yamate <fabioyamate@gmail.com>
2013-10-19 03:26:52 -0300

    Add omniauth columns to user :ambulance:

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131019062003) do
+ActiveRecord::Schema.define(version: 20131019062453) do
 
   create_table "projects", force: true do |t|
     t.string   "name",          null: false
@@ -44,6 +44,9 @@ ActiveRecord::Schema.define(version: 20131019062003) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
