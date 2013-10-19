ccda23919fb1a20befcc3935303652195190511b
Andriel Nuernberg <andriel.nuernberg@plataformatec.com.br>
2013-10-19 06:25:12 -0300

    Fix the :shit:

--- db/schema.rb
+++ db/schema.rb
@@ -14,11 +14,11 @@
 ActiveRecord::Schema.define(version: 20131019062453) do
 
   create_table "projects", force: true do |t|
-    t.string   "name",               null: false
+    t.string   "name",                                    null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "approval_emoji"
-    t.integer  "required_approvals"
+    t.string   "approval_emoji",     default: ":shipit:", null: false
+    t.integer  "required_approvals", default: 2,          null: false
   end
 
   create_table "repositories", force: true do |t|
