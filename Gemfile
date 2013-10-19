96f917074f21bfa74710aee60676476d1c2dc78d
Kassio Borges <kassioborgesm@gmail.com>
2013-10-18 21:24:47 -0300

    :postal_horn:

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.0.1.rc1'
+
+# Use mysql as the database for Active Record
+gem 'mysql2'
+
+# Use SCSS for stylesheets
+gem 'sass-rails', '~> 4.0.0'
+
+# Use Uglifier as compressor for JavaScript assets
+gem 'uglifier', '>= 1.3.0'
+
+# Use jquery as the JavaScript library
+gem 'jquery-rails'
+
+# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
+gem 'jbuilder', '~> 1.2'
+
+# Use Capistrano for deployment
+gem 'capistrano', group: :development
+
+gem 'rspec'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dashub::Application.config.secret_key_base = '243e516a6e12c7bc6306d86f5efcf54cab76bbbbf392e85f850cac215aa0d05fd9be74360d2288072b42dcfa6bea4c3af94020cab29912369b492d2becf4003d'
