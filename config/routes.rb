75c6d243096237719260e13923d959224851492f
Renato Mascarenhas <mascarenhas.renato@gmail.com>
2013-10-19 02:10:48 -0300

    Deny records removal. :abc:

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Dashub::Application.routes.draw do
   get 'welcome/index'
   root to: 'projects#index'
 
-  resources :projects do
-    resources :repositories
+  resources :projects, except: :destroy do
+    resources :repositories, except: :destroy
   end
 end
