6859a217087ddf5e76dd9ecc8273993be5605bdf
Kassio Borges <kassioborgesm@gmail.com>
2013-10-19 19:25:17 -0300

    move github url from view to helpers :ledger:

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def github_repository_url(repository)
+    "#{github_url}#{repository.fullname}"
+  end
+  alias :repo_url :github_repository_url
+
+  def github_url
+    "http://github.com/"
+  end
 end
