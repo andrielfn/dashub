9cdfbb15f94ca66cc112ac4547cd757591bd6230
Andriel Nuernberg <andriel.nuernberg@plataformatec.com.br>
2013-10-20 19:29:50 -0200

    Improve somethings :doughnut:

--- app/helpers/project_helper.rb
+++ app/helpers/project_helper.rb
@@ -5,10 +5,23 @@ module ProjectHelper
     elsif pull_request.reviewed?
       'reviewed'
     elsif pull_request.ready_to_ship?
-      'shipable'
+      'shippable'
     else
       'pending'
     end
+  end
+
+  def pull_request_status_description(pull_request)
+    pull_request_status = pull_request_status(pull_request)
+
+    descriptions = {
+      'approved' => 'Reviewed by you and ready to merge',
+      'reviewed' => 'Reviewed but waiting for your approval',
+      'shippable' => 'Ready to merge, but not reviewed by you',
+      'pending' => 'No one reviewed yet'
+    }
+
+    descriptions[pull_request_status]
   end
 
   def emoji_tag(name, url, options = {})
