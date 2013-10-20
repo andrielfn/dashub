1acf1b5e4e7c2ab25cd34444a9d40ae0d25e07ca
Fabio Yamate <fabioyamate@gmail.com>
2013-10-20 04:03:52 -0200

    Highlight selected suggested repo :end:

--- app/assets/javascripts/repository_selection.js
+++ app/assets/javascripts/repository_selection.js
@@ -1,8 +1,13 @@
 $(function($) {
+  var currentRepo;
+
   $('.suggested-repo').click(function() {
     var repositoryName  = $(this).text(),
         repositoryInput = $('#repository_full_name');
 
     repositoryInput.val(repositoryName);
+    $(currentRepo).removeClass('selected');
+    $(this).addClass('selected');
+    currentRepo = this;
   });
 });
