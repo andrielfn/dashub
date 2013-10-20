20f042d5aadaa52dd92367436c6e62867ddc7dc4
Renato Mascarenhas <mascarenhas.renato@gmail.com>
2013-10-20 01:25:51 -0200

    Truncate project name to 50 chars. :ru:

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 50 }
   validates :approval_emoji, presence: true
   validates :required_approvals, numericality: { only_integer: true, greater_than: 0 }
 
